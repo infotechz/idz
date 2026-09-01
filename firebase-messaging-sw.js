d2707b41e7ebefcd8479df32f5c6bf4be030d3ca
idz <olliveirazvz@gmail.com>
2026-09-01 08:46:49 -0300

    Fase 1: padroniza usuarios por UID e seguranca Firebase

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBAQ1HRInFDHnOERD3iD9lifBcJAhRWI1c',
+  authDomain: 'informatica-do-zero-40928.firebaseapp.com',
+  projectId: 'informatica-do-zero-40928',
+  storageBucket: 'informatica-do-zero-40928.firebasestorage.app',
+  messagingSenderId: '700791806827',
+  appId: '1:700791806827:web:9556d2ee88f19dd5c9ca2e'
+});
+
+firebase.messaging();
